6f4489b4381af26f370d04e3a763d8d8d6bf9cfc
Key Clark <aryeh.the.lion@gmail.com>
2023-11-05 01:18:44 -0600

    register error handler with server main

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/materdev/golang_test-server-gin/src/api/middlewares"
 	"github.com/materdev/golang_test-server-gin/src/api/routes"
 )
 
 func main() {
 	r := gin.Default()
+
+	// Register global error handling middleware
+	r.Use(middlewares.ErrorHandler)
 	
 	// Handlers
 	routes.RegisterHealthRoutes(r)
